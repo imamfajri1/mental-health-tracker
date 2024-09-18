7361dbba10107536e528635d855690eee585c11b
Imam Fajri <imamfajri.ze@gmail.com>
2024-09-18 13:33:20 +0700

    Autentikasi, Session, dan Cookies Done

--- mental_health_tracker/settings.py
+++ mental_health_tracker/settings.py
@@ -9,7 +9,7 @@ https://docs.djangoproject.com/en/5.1/topics/settings/
 For the full list of settings and their values, see
 https://docs.djangoproject.com/en/5.1/ref/settings/
 """
-
+import os
 from pathlib import Path
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
@@ -23,7 +23,8 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = 'django-insecure-###@b@3#sypq5uu4-p=l*&uetj+tps2d3vqiu=f=v#t$n$q-fs'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+PRODUCTION = os.getenv("PRODUCTION", False)
+DEBUG = not PRODUCTION
 
 ALLOWED_HOSTS = ["localhost", "127.0.0.1", "imam-fajri-mentalhealthtracker.pbp.cs.ui.ac.id"]
 
